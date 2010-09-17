25d0278b4142dea35d73be86d22258d74739dd82
Simon Schmeißer <s.schmeisser@gmx.net>
2010-09-17 17:43:39 +0200

    farpock auswertung ++

--- farpock/numbers.py
+++ farpock/numbers.py
@@ -3,9 +3,16 @@
 
 def number_to_latex(number, precission, base):
   return r'' + str(round(number/(10**base), precission)) + r'\e{' + str(base) + r'}'
-  
+
+def n_to_latex(number, precission, base):
+  return r'' + str(round(number/(10**base), precission))
+
 def number_error_to_latex(number, error, precission, base):
-  return r'(' + str(round(number/(10**base), precission)) + r' \pm ' + str(round(error/(10**base), precission)) + r')\e{' + str(base) + r'}'
-  
+  return r'$(' + str(round(number/(10**base), precission)) + r' \pm ' + str(round(error/(10**base), precission)) + r')\e{' + str(base) + r'}$'
+
+def n_e_to_latex(number, error, precission, base):
+  return r'$(' + str(round(number/(10**base), precission)) + r' \pm ' + str(round(error/(10**base), precission)) + r')$'
+
+
 #print number_to_latex(0.0001234567890, 3, -5)
 #print number_error_to_latex(0.0001234567890, 1e-6, 3, -5)
